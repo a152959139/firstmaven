a7f37f45f0226e4d40861b3a0f205456a9871070
a152959139 <2505886264@qq.com>
2020-04-27 17:30:02 +0800

    第一次提交

---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+import org.apache.commons.lang.StringUtils;
+
+public class main {
+
+    public static void main(String[] args){
+        String str = "swaldjasdasnklajsw";
+        if(StringUtils.isNotBlank(str))
+            System.out.println("this is not a blank String");
+        else
+            System.out.println("this is  a blank String");
+    }
+}
